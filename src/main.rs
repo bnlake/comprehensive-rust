045892e7f138f80cebd90cd77ff5de0484fcab3f
Brian Lake <bnlake@gmail.com>
2023-06-29 07:09:21 -0500

    different string slices

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 fn main() {
-    let a: [i32; 8] = [10, 20, 30, 40, 50, 60, 70, 80];
-    println!("a: {a:?}");
+    // &str is a reference to a string slice
+    // Writing literal strings returns a reference to a string slice
+    let s1: &str = "World";
+    println!("String 1: {s1}");
 
-    // Slices borrow data from the sliced type
-    let slice = &a[2..5];
-    println!("slice: {slice:?}");
+    // String is a string "buffer"
+    // The String type is a value stored on the stack
+    // It maintains 3 parts of information about the string:
+    //      1. A pointer to some bytes
+    //      2. A length
+    //      3. A capacity
+    let mut s2 = String::from("Hello");
+    println!("String 2: {s2}");
+    s2.push_str(s1);
+    println!("String 2: {s2}");
+
+    // A slice can be taken of a slice
+    let s3: &str = &s1[0..2];
+    println!("String 3: {s3}");
+    
+    // A slice can be taken from a string buffer
+    let s4: &str = &s2[5..];
+    println!("String 4: {s4}");
 }
