67147a3195d7ff88b2eb592662d72f9f52575f35
Brian Lake <bnlake@gmail.com>
2023-06-29 06:53:47 -0500

    borrow a slice of an array

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let a: [i32; 8] = [10, 20, 30, 40, 50, 60, 70, 80];
+    println!("a: {a:?}");
+
+    // Slices borrow data from the sliced type
+    let slice = &a[2..5];
+    println!("slice: {slice:?}");
 }
