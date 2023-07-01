64776e6e9f5de5d31382c778738565b7bf44a005
Brian Lake <bnlake@gmail.com>
2023-06-30 21:57:14 -0500

    deriving the Copy trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Debug)]
+
+struct Point(i32, i32);
+
+#[derive(Debug, Clone, Copy)]
+struct CopyPoint(i32, i32);
+
 fn main() {
-    println!("Hello, world!");
+    let p1 = Point(2, 5);
+    let p2 = p1;
+    // The following line would throw a compile time error
+    // This is because p1 is not a native type that implements the Copy trait
+    // therefore it is moved into p2
+    // println!("{p1:?}")
+
+    let p3 = CopyPoint(2, 5);
+    let p4 = p3;
+    // Now it's ok because the value is implicitly copied into p4
+    println!("{p4:?}")
 }
