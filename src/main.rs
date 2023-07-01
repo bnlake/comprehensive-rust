412eaaa8de06b2bbb9e2a4b0ebb0d6f9f193cf41
Brian Lake <bnlake@gmail.com>
2023-06-30 22:30:35 -0500

    lifetime on data structures

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
-#[derive(Debug)]
-struct Point(i32, i32);
+use std::fmt::Display;
 
-/// I believe variables that share the same lifetime specifier
-/// is the same as telling the compiler that variable a, variable b
-/// and the result of this function must all live at least the same
-/// length at application runtime
-fn left_most<'a>(a: &'a Point, b: &'a Point) -> &'a Point {
-    if a.0 < b.0 {
-        a
-    } else {
-        b
+#[derive(Debug)]
+struct Highlight<'a>(&'a str);
+impl Display for Highlight<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "\"{}\"", self.0)
     }
 }
 
 fn main() {
-    let a = Point(12, 53);
-    let b = Point(82, 14);
-    let left_point = left_most(&a, &b);
-    println!("The lest most point is {left_point:?}");
+    let phrase = String::from("Here is a really long string");
+    let highlight = Highlight(&phrase[10..21]);
+    println!("The highlighted text: {highlight}");
 }
