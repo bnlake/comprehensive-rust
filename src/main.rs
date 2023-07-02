bdd178d502f632a8a35d8b209032a17e52bdc174
Brian Lake <bnlake@gmail.com>
2023-07-01 21:52:55 -0500

    luhn algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,68 @@
-#[derive(Debug)]
-enum LinkedList<T> {
-    Cons(T, Box<LinkedList<T>>),
-    Nil,
-}
-impl<T> LinkedList<T> {
-    fn new(t: T) -> LinkedList<T> {
-        LinkedList::Cons(t, Box::new(LinkedList::Nil))
-    }
+#![allow(unused_variables, dead_code)]
 
-    fn new_with_next(t: T, next: LinkedList<T>) -> LinkedList<T> {
-        LinkedList::Cons(t, Box::new(next))
+pub fn luhn(cc_number: &str) -> bool {
+    let mut digits: Vec<u32> = cc_number
+        .chars()
+        .filter(|c| c.is_ascii_digit())
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    if digits.len() < 2 {
+        false
+    } else {
+        digits.reverse();
+        let result: u32 = digits
+            .iter()
+            .enumerate()
+            .map(|(i, d)| {
+                if i % 2 == 0 {
+                    *d
+                } else {
+                    let double = d * 2;
+                    double / 10 + double % 10
+                }
+            })
+            .sum();
+        result % 10 == 0
     }
 }
 
-fn main() {
-    let list: LinkedList<i32> =
-        LinkedList::new_with_next(24, LinkedList::new_with_next(12, LinkedList::Nil));
-    println!("{list:?}")
+#[test]
+fn test_non_digit_cc_number() {
+    assert!(!luhn("foo"));
 }
+
+#[test]
+fn test_empty_cc_number() {
+    assert!(!luhn(""));
+    assert!(!luhn(" "));
+    assert!(!luhn("  "));
+    assert!(!luhn("    "));
+}
+
+#[test]
+fn test_single_digit_cc_number() {
+    assert!(!luhn("0"));
+}
+
+#[test]
+fn test_two_digit_cc_number() {
+    assert!(luhn(" 0 0 "));
+}
+
+#[test]
+fn test_valid_cc_number() {
+    assert!(luhn("4263 9826 4026 9299"));
+    assert!(luhn("4539 3195 0343 6467"));
+    assert!(luhn("7992 7398 713"));
+}
+
+#[test]
+fn test_invalid_cc_number() {
+    assert!(!luhn("4223 9826 4026 9299"));
+    assert!(!luhn("4539 3195 0343 6476"));
+    assert!(!luhn("8273 1232 7352 0569"));
+}
+
+#[allow(dead_code)]
+fn main() {}
