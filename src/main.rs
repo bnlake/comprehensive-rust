030099c1136e0eba00307f1f85374008497af22d
Brian Lake <blake@krames.com>
2023-07-03 10:08:55 -0500

    specify closure as param trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+fn apply_with_log<F>(func: F, input: i32) -> i32
+where
+    F: FnOnce(i32) -> i32,
+{
+    println!("Calling function on {input}");
+    func(input)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let add_3 = |x| x + 3;
+    println!("add_3: {}", apply_with_log(add_3, 10));
+    println!("add_3: {}", apply_with_log(add_3, 20));
+
+    let mut v = Vec::new();
+    let mut acc = |x: i32| {
+        v.push(x);
+        v.iter().sum::<i32>()
+    };
+    println!("accumulate: {}", apply_with_log(&mut acc, 4));
+    println!("accumulate: {}", apply_with_log(&mut acc, 5));
+
+    let multiply_sum = |x| x * v.into_iter().sum::<i32>();
+    println!("multiply_sum: {}", apply_with_log(multiply_sum, 3));
 }
