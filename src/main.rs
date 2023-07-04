e95331c8bbfefdb9789007041f4766f142a4bce9
Brian Lake <bnlake@gmail.com>
2023-07-03 20:21:12 -0500

    spawn a thread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{thread, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    thread::spawn(|| {
+        for i in 0..10 {
+            println!(
+                "Count in thread {}: {}",
+                thread::current().name().unwrap_or("separate thread"),
+                i
+            );
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for i in 0..10 {
+        println!(
+            "Count in thread {}: {}",
+            thread::current().name().unwrap(),
+            i
+        );
+        thread::sleep(Duration::from_secs(1));
+    }
 }
