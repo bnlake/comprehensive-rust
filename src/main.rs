4e5f9d84f3149392cd0e0abf03cc93c4ac36382d
Brian Lake <bnlake@gmail.com>
2023-07-05 22:15:27 -0500

    use mutex and arc to read and mutate shared state

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::sync::Mutex;
+use std::{
+    sync::{Arc, Mutex},
+    thread::{self, JoinHandle},
+    time::Duration,
+};
+
+fn spawn_thread(v: &Arc<Mutex<Vec<i32>>>, delay: i32) -> JoinHandle<()> {
+    let v = Arc::clone(v);
+    thread::spawn(move || {
+        thread::sleep(Duration::from_millis(delay as u64));
+        let mut guard = v.lock().unwrap();
+        guard.push(delay);
+    })
+}
 
 fn main() {
-    let m = Mutex::new(vec![10, 20, 30]);
-    println!("m: {:?}", m.lock().unwrap());
-    {
-        let mut guard = m.lock().unwrap();
-        guard.push(40);
+    let m = Arc::new(Mutex::new(vec![10, 20, 30]));
+    let mut handles = Vec::new();
+
+    for i in 1..5 {
+        handles.push(spawn_thread(&m, i * 200));
     }
+
+    handles.into_iter().for_each(|h| h.join().unwrap());
     println!("m: {:?}", m.lock().unwrap());
 }
